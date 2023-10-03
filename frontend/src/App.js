359fcec91fb76e5356326d1f47b4ce6f245b0633
Dhruvpatel007 <dhruvdpatel0709@gmail.com>
2023-10-03 10:42:25 +0530

    Header complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './Components/Header'
 
 const App = () => {
   return (
-    <div>Hello Shopwale!!!</div>
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+      <h1>Hello Shopwale!!!</h1>
+      </Container>
+    </main>
+    </>
   )
 }
 
